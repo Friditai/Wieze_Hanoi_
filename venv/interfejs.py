a1dcf09145254b651cbbd3e44e6ddab31927a038
Friditai <m.wesiora@gmail.com>
2019-12-18 14:53:29 +0100

    Rozpoczęcie pracy nad interfejsem

--- venv/interfejs.py
+++ venv/interfejs.py
@@ -36,6 +36,7 @@ class Palette:
         self.BLACK = (0, 0, 0)
         self.WHITE = (255, 255, 255)
         self.BROWN = (139, 69, 19)
+        self.GREY = (91, 93, 116)
 
 # obiekt klasy Palette
 col = Palette()
@@ -48,14 +49,33 @@ while True:
              sys.exit(0)
            #kolor tla
         #GAME_SURFACE.fill(col.GREEN)
-     tlo = pygame.image.load("IMG_2012png.png").convert()
+     tlo = pygame.image.load("tlo1m.png").convert()
         # pobieramy informacje o ekranie - tle
      screen = pygame.display.get_surface()
 
 #wieże
-     wieza1 = pygame.draw.rect(tlo, col.BROWN, pygame.Rect(310,500,15,170))
+     wieza1 = pygame.draw.rect(tlo, col.BROWN, pygame.Rect(310, 500, 15, 170))
+     podstawa1 = pygame.draw.rect(tlo, col.BROWN, pygame.Rect(290, 670, 55, 10))
      wieza2 = pygame.draw.rect(tlo, col.BROWN, pygame.Rect(620, 500, 15, 170))
+     podstawa2 = pygame.draw.rect(tlo, col.BROWN, pygame.Rect(600, 670, 55, 10))
      wieza3 = pygame.draw.rect(tlo, col.BROWN, pygame.Rect(930, 500, 15, 170))
+     podstawa3 = pygame.draw.rect(tlo, col.BROWN, pygame.Rect(910, 670, 55, 10))
+
+     # wymiary krążków
+     g = 17  # grubość krążka
+     d1 = 155  # długość krążka nr 1 - najdłuższego
+     d2 = 135
+     d3 = 115
+     d4 = 95
+     d5 = 75
+
+     # krążki na 1 wieży - stan początkowy
+
+     krazek1A = pygame.draw.rect(tlo, col.GREY, pygame.Rect(240, 667, 155, 17))
+     krazek2A = pygame.draw.rect(tlo, col.GREY, pygame.Rect(250, 650, 135, 17))
+     krazek3A = pygame.draw.rect(tlo, col.GREY, pygame.Rect(260, 633, 115, 17))
+     krazek4A = pygame.draw.rect(tlo, col.GREY, pygame.Rect(270, 616, 95, 17))
+     krazek5A = pygame.draw.rect(tlo, col.GREY, pygame.Rect(280, 599, 75, 17))
 
 
 
